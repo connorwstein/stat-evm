3d8f1441614e7976311814daf84b32284b81fd62
A.Tena <tena@subharmonic.tech>
2022-08-07 14:35:35 -0400

    Demo ready

--- scripts/precompile/main.go
+++ scripts/precompile/main.go
@@ -61,7 +61,7 @@ func start_events(ec *ethclient.Client, addr common.Address) {
 }
 
 func main() {
-	ec, err := ethclient.Dial("http://127.0.0.1:11739/ext/bc/pzcofHDVjZWS6HgZEj7cvXtRvsSzuqhupBMcdrzB2QTuG7kWz/rpc")
+	ec, err := ethclient.Dial("http://127.0.0.1:25937/ext/bc/v69EbkeBrb2RLJ3AzSAK884mX5vVJ7BR9cJa7E5Yhe8U7aaR6/rpc")
 	panicErr(err)
 
 	b, err := ec.ChainID(context.Background())
@@ -80,32 +80,32 @@ func main() {
 
 	confirm(ec, deployTx.Hash())
 
-	user.GasLimit = 500_000
-	tx, err := testContract.TestMedian(user, []*big.Int{big.NewInt(5), big.NewInt(10), big.NewInt(3), big.NewInt(7), big.NewInt(12)})
-	panicErr(err)
-	confirm(ec, tx.Hash())
-	// fmt.Println("Tx hash (median):", tx.Hash())
-	l, err := testContract.Med(nil)
-	panicErr(err)
-	fmt.Println("median", l, err)
+	// user.GasLimit = 500_000
+	// tx, err := testContract.TestMedian(user, []*big.Int{big.NewInt(5), big.NewInt(10), big.NewInt(3), big.NewInt(7), big.NewInt(12)})
+	// panicErr(err)
+	// confirm(ec, tx.Hash())
+	// // fmt.Println("Tx hash (median):", tx.Hash())
+	// l, err := testContract.Med(nil)
+	// panicErr(err)
+	// fmt.Println("median", l, err)
 
-	sampler_tx, err := testContract.TestSampler(user, big.NewInt(0), big.NewInt(0), big.NewInt(1000000000000000000), big.NewInt(5))
-	panicErr(err)
-	confirm(ec, sampler_tx.Hash())
-	// fmt.Println("Tx hash (sampler):", sampler_tx.Hash())
+	// sampler_tx, err := testContract.TestSampler(user, big.NewInt(0), big.NewInt(0), big.NewInt(1000000000000000000), big.NewInt(5))
+	// panicErr(err)
+	// confirm(ec, sampler_tx.Hash())
+	// // fmt.Println("Tx hash (sampler):", sampler_tx.Hash())
 
-	sampler_res, err := testContract.GetLastSample(nil)
-	panicErr(err)
-	fmt.Println("sample result", sampler_res, err)
+	// sampler_res, err := testContract.GetLastSample(nil)
+	// panicErr(err)
+	// fmt.Println("sample result", sampler_res, err)
 
-	moment_tx, err := testContract.TestMoment(user, big.NewInt(2), []*big.Int{big.NewInt(23), big.NewInt(31), big.NewInt(23)}, []*big.Int{big.NewInt(1), big.NewInt(1), big.NewInt(1)})
-	panicErr(err)
-	confirm(ec, moment_tx.Hash())
-	// fmt.Println("Tx hash (moment_tx):", moment_tx.Hash())
+	// moment_tx, err := testContract.TestMoment(user, big.NewInt(2), []*big.Int{big.NewInt(23), big.NewInt(31), big.NewInt(23)}, []*big.Int{big.NewInt(1), big.NewInt(1), big.NewInt(1)})
+	// panicErr(err)
+	// confirm(ec, moment_tx.Hash())
+	// // fmt.Println("Tx hash (moment_tx):", moment_tx.Hash())
 
-	moment_res, err := testContract.Moment(nil)
-	panicErr(err)
-	fmt.Println("moment result", moment_res, err)
+	// moment_res, err := testContract.Moment(nil)
+	// panicErr(err)
+	// fmt.Println("moment result", moment_res, err)
 
 	price_prediction_tx, err := testContract.TestIPFSPrediction(user, "QmZbV8nB1uQf5NqhsXB8RhGNJpUr4v92Br4jw6a7MdExvz", big.NewInt(10), big.NewInt(10))
 	panicErr(err)
@@ -117,39 +117,39 @@ func main() {
 	panicErr(err)
 	fmt.Println("price prediction result", price_prediction_res, err)
 
-	var a [][]*big.Int
-	for i := 0; i < 1; i++ {
-		a = append(a, []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(3)})
-	}
-	var b2 [][]*big.Int
-	for i := 0; i < 3; i++ {
-		b2 = append(b2, []*big.Int{big.NewInt(int64(i + 1))})
-	}
-	matrix_tx, err := testContract.TestMatrixMult(user, a, b2)
-	panicErr(err)
-	confirm(ec, matrix_tx.Hash())
-	fmt.Println("Tx hash (matrix):", matrix_tx.Hash())
+	// var a [][]*big.Int
+	// for i := 0; i < 1; i++ {
+	// 	a = append(a, []*big.Int{big.NewInt(1), big.NewInt(2), big.NewInt(3)})
+	// }
+	// var b2 [][]*big.Int
+	// for i := 0; i < 3; i++ {
+	// 	b2 = append(b2, []*big.Int{big.NewInt(int64(i + 1))})
+	// }
+	// matrix_tx, err := testContract.TestMatrixMult(user, a, b2)
+	// panicErr(err)
+	// confirm(ec, matrix_tx.Hash())
+	// fmt.Println("Tx hash (matrix):", matrix_tx.Hash())
 
-	vals, err := testContract.GetMatrixMulti(nil)
-	panicErr(err)
-	fmt.Println(vals)
+	// vals, err := testContract.GetMatrixMulti(nil)
+	// panicErr(err)
+	// fmt.Println(vals)
 
-	var x [][]*big.Int
-	var y [][]*big.Int
-	for i := 0; i < 100; i++ {
-		val := big.NewInt(int64(i + 1))
-		val_times_two := new(big.Int).Mul(val, big.NewInt(2))
-		x = append(x, []*big.Int{val})
-		y = append(y, []*big.Int{val_times_two})
-	}
+	// var x [][]*big.Int
+	// var y [][]*big.Int
+	// for i := 0; i < 100; i++ {
+	// 	val := big.NewInt(int64(i + 1))
+	// 	val_times_two := new(big.Int).Mul(val, big.NewInt(2))
+	// 	x = append(x, []*big.Int{val})
+	// 	y = append(y, []*big.Int{val_times_two})
+	// }
 
-	fitType := big.NewInt(0) // 0 = OLS, 1 = LASSO
-	fit_tx, err := testContract.TestFit(user, fitType, x, y)
-	panicErr(err)
-	confirm(ec, fit_tx.Hash())
-	fmt.Println("Tx hash (fit):", fit_tx.Hash())
+	// fitType := big.NewInt(0) // 0 = OLS, 1 = LASSO
+	// fit_tx, err := testContract.TestFit(user, fitType, x, y)
+	// panicErr(err)
+	// confirm(ec, fit_tx.Hash())
+	// fmt.Println("Tx hash (fit):", fit_tx.Hash())
 
-	fit_vals, err := testContract.GetFitted(nil)
-	panicErr(err)
-	fmt.Println(fit_vals)
+	// fit_vals, err := testContract.GetFitted(nil)
+	// panicErr(err)
+	// fmt.Println(fit_vals)
 }
